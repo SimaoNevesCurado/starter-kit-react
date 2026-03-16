21ef8145804a3fc905f4f8802d764ff9d82acbfd
simaonevescurado <simao.curadoo@gmail.com>
2026-03-16 16:50:40 +0000

    Prepare starter kit for Inertia v3

--- config/inertia.php
+++ config/inertia.php
@@ -40,16 +40,17 @@ return [
         'ensure_pages_exist' => true,
 
         'page_paths' => [
-            resource_path('js/pages'),
-        ],
-
-        'page_extensions' => [
-            'js',
-            'jsx',
-            'svelte',
-            'ts',
-            'tsx',
-            'vue',
+            [
+                'path' => resource_path('js/pages'),
+                'extensions' => [
+                    'js',
+                    'jsx',
+                    'svelte',
+                    'ts',
+                    'tsx',
+                    'vue',
+                ],
+            ],
         ],
 
     ],
